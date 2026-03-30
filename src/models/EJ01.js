d49f3c3ff32d098b8ce799947a7a6d6f51063654
vickytandeter <49004038@est.ort.edu.ar>
2026-03-30 09:10:43 -0300

    Add files via upload

---
+++ src/models/EJ01.js
@@ -0,0 +1,7 @@
+let nombre = "Flor"
+let apellido = "Strotinsky"
+
+function ejercicio01(nombre, apellido)
+{
+    console.log(`Nombre: ${nombre} Apellido: ${apellido}`)
+}
---
+++ src/modules/matematica.js
@@ -0,0 +1,9 @@
+const PI = 3.14
+
+const sumar = (a, b) => a + b;
+
+const resto = (a, b) => a - b;
+
+const multiplicacion = (a, b) => a * b;
+
+const division = (a, b) => a / b;
