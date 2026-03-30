23ed4d5345d6e31d4ad4497df55ed27f50370e4b
VICTORIA TANDETER <49004038@ort>
2026-03-30 10:22:56 -0300

    Ej02

---
+++ src/EJ02.js
@@ -0,0 +1,9 @@
+import {PI, sumar} from './src/modules/matematica.js';
+
+let num1 = 10
+let num2 = 2
+let total = sumar(num1, num2)
+
+console.log(`Pi vale ${PI}`)
+console.log(`${num1} + ${num2} = ${total}`)
+
--- src/models/EJ01.js
+++
@@ -1,7 +0,0 @@
-let nombre = "Flor"
-let apellido = "Strotinsky"
-
-function ejercicio01(nombre, apellido)
-{
-    console.log(`Nombre: ${nombre} Apellido: ${apellido}`)
-}
